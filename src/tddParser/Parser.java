066a4ce164cc118ab8ad75a79bdba2e97e9a39cd
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-22 14:34:05 -0300

    Add Falsifications tdd method on ReadFile.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/tddParser/Parser.java
@@ -0,0 +1,16 @@
+package tddParser;
+
+public class Parser {
+	private int file;
+	
+	public Parser() {
+	}
+	public void readFile (String fileName) {
+		this.file = 1337;
+	}
+	
+	public int getFile() {
+		return this.file;
+	}
+	
+}
--- src/tst/ReadFileTst.java
+++ src/tst/ReadFileTst.java
@@ -1,14 +1,20 @@
 package tst;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-class ReadFileTst {
+import tddParser.Parser;
 
+public class ReadFileTst {	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void TestReadFileSuccess() {
+		Parser parser = new Parser();
+		parser.readFile("");
+		assertEquals(1337, parser.getFile(), 0);
 	}
 
 }
