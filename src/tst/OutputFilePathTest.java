da1033d82946099836e0dea88289c5d7bceddb54
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-23 08:53:47 -0300

    Add failure output file path test.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/tst/OutputFilePathTest.java
@@ -0,0 +1,14 @@
+package tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OutputFilePathTest {
+
+	@Test
+	void TestOutputFilePath() {
+		fail("Not yet implemented");
+	}
+
+}
