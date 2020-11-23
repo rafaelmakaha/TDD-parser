57194237f9c25b2bd5ddf20abbc025407c71f6b2
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-22 23:26:19 -0300

    Add Output sequence format failure test.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/tst/OutputSequenceFormatTest.java
@@ -0,0 +1,14 @@
+package tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OutputSequenceFormatTest {
+
+	@Test
+	void TestOutputSequenceFormatTest() {
+		fail("Not yet implemented");
+	}
+
+}
