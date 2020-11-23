ccef7cd69f2cdad4bd587dfc62c8221829652145
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-22 21:10:56 -0300

    Add failure test delimiter.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/tst/DelimiterTst.java
@@ -0,0 +1,14 @@
+package tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DelimiterTst {
+
+	@Test
+	void TestDelimiterFail() {
+		fail("Not yet implemented");
+	}
+
+}
