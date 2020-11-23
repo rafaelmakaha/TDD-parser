758ee834a296c6c419d807ec0409354ca3b3f6a8
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-22 23:14:55 -0300

    Add failure write file test case.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ src/tst/WriteFileTst.java
@@ -0,0 +1,14 @@
+package tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WriteFileTst {
+
+	@Test
+	void TestWriteFile() {
+		fail("Not yet implemented");
+	}
+
+}
