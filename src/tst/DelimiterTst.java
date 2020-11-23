e587b72ebe1b197ef7d91abf4447663265f61e82
Rafael Makaha <rafael.makaha@gmail.com>
2020-11-22 22:39:58 -0300

    Add before all test method.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- src/tst/DelimiterTst.java
+++ src/tst/DelimiterTst.java
@@ -2,38 +2,40 @@ package tst;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import app.Parser;
 import exceptions.DelimitadorInvalidoException;
 
 class DelimiterTst {
+	Parser parser;
+	
+	@BeforeEach
+	public void setup() {
+		parser = new Parser();
+	}
 
 	@Test
 	public void TestDelimiterSuccess () throws DelimitadorInvalidoException {
-		Parser parser = new Parser();
 		parser.setDelimiter(";");
 		assertEquals(";", parser.getDelimiter());
 	}
 	
 	@Test
 	public void TestDelimiterSuccess2 () throws DelimitadorInvalidoException {
-		Parser parser = new Parser();
 		parser.setDelimiter(",");
 		assertEquals(",", parser.getDelimiter());
 	}
 	
 	@Test 
 	public void TestDelimiterSuccess3 () throws DelimitadorInvalidoException {
-		Parser parser = new Parser();
 		parser.setDelimiter("g");
 		assertEquals("g", parser.getDelimiter());
 	}
 	
 	@Test
 	public void TestDelimitadorInvalidoException () {
-		Parser parser = new Parser();
-		
 		assertThrows(DelimitadorInvalidoException.class, () -> {
 			parser.setDelimiter("ab");
 		});
